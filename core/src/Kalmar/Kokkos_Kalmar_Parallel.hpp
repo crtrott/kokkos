a59b14ed2e8758cb74977fa6aba51f35962245be
Christian Trott <crtrott@sandia.gov>
2015-06-05 10:19:20 -0500

    Kalmar: Initial attempt at putting in TeamPolicy support
    This doesn't work yet and has issue related to referencing outside
    module.
    In a couple of cases we worked around this wiht generating auto lambdas.

--- core/src/Kalmar/Kokkos_Kalmar_Parallel.hpp
+++ core/src/Kalmar/Kokkos_Kalmar_Parallel.hpp
@@ -44,7 +44,79 @@
 #include <Kalmar/Kokkos_Kalmar_Reduce.hpp>
 
 namespace Kokkos {
+
+  struct kalmar_team_member_type {
+    enum {TEAM_SIZE = 256 }; 
+    typedef TeamPolicy<Kokkos::Kalmar,void,Kokkos::Kalmar> TeamPolicy;
+    KOKKOS_INLINE_FUNCTION int league_rank() const ;
+    KOKKOS_INLINE_FUNCTION int league_size() const { return m_league_size ; }
+    KOKKOS_INLINE_FUNCTION int team_rank() const ;
+    KOKKOS_INLINE_FUNCTION int team_size() const { return TEAM_SIZE ; }
+
+    KOKKOS_INLINE_FUNCTION
+    kalmar_team_member_type( const TeamPolicy & arg_policy
+               , const concurrency::tiled_index< TEAM_SIZE > & arg_idx )
+      : m_league_size( arg_policy.league_size() )
+      , m_league_rank( arg_idx.tile[0]  )
+      , m_team_rank( arg_idx.local[0] )
+      {}
+
+  private:
+    int m_league_size ;
+    int m_league_rank ;
+    int m_team_rank ;
+  };
+
+template< class Arg0 , class Arg1 >
+class TeamPolicy< Arg0 , Arg1 , Kokkos::Kalmar > {
+public:
+  enum { TEAM_SIZE = 256 };
+  int m_league_size ;
+
+  using execution_policy = TeamPolicy ;
+  using execution_space  = Kokkos::Kalmar ;
+  using work_tag         = void ;
+
+  TeamPolicy( const int arg_league_size
+            , const int arg_team_size )
+    : m_league_size( arg_league_size )
+    {}
+
+  KOKKOS_INLINE_FUNCTION int team_size() const { return TEAM_SIZE ; }
+  KOKKOS_INLINE_FUNCTION int league_size() const { return m_league_size ; }
+
+  //This is again a reference thing from other module error 
+  //We used auto last time to work around it.
+  /*
+  struct member_type {
+    KOKKOS_INLINE_FUNCTION int league_rank() const ;
+    KOKKOS_INLINE_FUNCTION int league_size() const { return m_league_size ; }
+    KOKKOS_INLINE_FUNCTION int team_rank() const ;
+    KOKKOS_INLINE_FUNCTION int team_size() const { return TEAM_SIZE ; }
+
+    KOKKOS_INLINE_FUNCTION
+    member_type( const TeamPolicy & arg_policy
+               , const concurrency::tiled_index< TEAM_SIZE > & arg_idx )
+      : m_league_size( arg_policy.league_size() )
+      , m_league_rank( arg_idx.tile[0]  )
+      , m_team_rank( arg_idx.local[0] )
+      {}
+
+  private:
+    int m_league_size ;
+    int m_league_rank ;
+    int m_team_rank ;
+  };
+  */
+  typedef kalmar_team_member_type member_type;
+};
+
+} // namespace Kokkos
+
+namespace Kokkos {
 namespace Impl {
+
+//----------------------------------------------------------------------------
 
 template< class FunctorType , class Arg0 , class Arg1 , class Arg2 >
 class ParallelFor< FunctorType
@@ -80,6 +152,40 @@ public:
 
     }
 };
+
+//----------------------------------------------------------------------------
+
+template< class FunctorType , class Arg0 , class Arg1 >
+class ParallelFor< FunctorType
+                 , Kokkos::TeamPolicy< Arg0 , Arg1 , Kokkos::Kalmar > >
+{
+  using Policy = Kokkos::TeamPolicy< Arg0 , Arg1 , Kokkos::Kalmar > ;
+  FunctorType m_functor ;
+public:
+  inline
+  ParallelFor( const FunctorType & functor
+             , const Policy      & policy )
+    :m_functor(functor)
+    {
+      auto make_lambda =
+        [&]( const concurrency::tiled_index< 256 > & idx ) restrict(amp)
+      {
+        using member_type = typename Policy::member_type ;
+        
+        this->m_functor( kalmar_team_member_type( policy , idx ) );
+      };
+
+      concurrency::extent< 1 >
+        flat_extent( policy.league_size() * 256 );
+
+      concurrency::tiled_extent< 256 > team_extent =
+        flat_extent.tile< 256 >();
+
+      concurrency::parallel_for_each( team_extent , make_lambda );
+    }
+};
+
+//----------------------------------------------------------------------------
 
 template< class FunctorType , class Arg0 , class Arg1 , class Arg2 >
 class ParallelReduce<
