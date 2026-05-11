a4cc36cd3d3f5cb17a2070f0a2d728bcd13f7706
Christian Trott <crtrott@sandia.gov>
2026-05-11 08:20:16 -0600

    Add more View ctor ptr conversion test for nullptr
    Signed-off-by: Christian Trott <crtrott@sandia.gov>

--- core/unit_test/view/TestViewCtorConvertibleToPtr.hpp
+++ core/unit_test/view/TestViewCtorConvertibleToPtr.hpp
@@ -11,10 +11,12 @@ import kokkos.core_impl;
 #include <Kokkos_Core.hpp>
 #endif
 
+#define N 3
+
 namespace {
 
 struct ArrayLike {
-  double data[3];
+  double data[N];
   KOKKOS_FUNCTION
   operator double*() { return data; }
   KOKKOS_FUNCTION
@@ -24,24 +26,24 @@ struct ArrayLike {
 // clang-format off
 // clang-format gets confused, and thinks the lambda closure brace is the namespace
 void from_array_like() {
-  Kokkos::View<ArrayLike*, TEST_EXECSPACE> a("A", 2);
+  Kokkos::View<ArrayLike, TEST_EXECSPACE> a("A");
   int errors = 0;
   Kokkos::parallel_reduce(Kokkos::RangePolicy(TEST_EXECSPACE(), 0, 1), KOKKOS_LAMBDA(int, int& err) {
     {
-      Kokkos::View<double*> b(a(0), 3);
-      if(b.data() != &a(0).data[0]) err++;
+      Kokkos::View<double*, TEST_EXECSPACE> b(a(), N);
+      if(b.data() != &a().data[0]) err++;
     }
     {
-      Kokkos::View<double*, Kokkos::MemoryUnmanaged> b(a(0), 3);
-      if (b.data() != &a(0).data[0]) err++;
+      Kokkos::View<double*, TEST_EXECSPACE, Kokkos::MemoryUnmanaged> b(a(), N);
+      if (b.data() != &a().data[0]) err++;
     }
     {
-      Kokkos::View<const double*> b(a(0), 3);
-      if (b.data() != &a(0).data[0]) err++;
+      Kokkos::View<const double*, TEST_EXECSPACE> b(a(), N);
+      if (b.data() != &a().data[0]) err++;
     }
     {
-      Kokkos::View<const double*, Kokkos::MemoryUnmanaged> b(a(0), 3);
-      if (b.data() != &a(0).data[0]) err++;
+      Kokkos::View<const double*, TEST_EXECSPACE, Kokkos::MemoryUnmanaged> b(a(), N);
+      if (b.data() != &a().data[0]) err++;
     }
   }, errors);
   ASSERT_EQ(errors, 0);
@@ -53,21 +55,23 @@ void from_carray() {
   Kokkos::parallel_reduce(
       Kokkos::RangePolicy(TEST_EXECSPACE(), 0, 1),
       KOKKOS_LAMBDA(int, int& err) {
-        double a[3];
+        double a[N];
         {
-          Kokkos::View<double*> b(a, 3);
+          Kokkos::View<double*, TEST_EXECSPACE> b(a, N);
           if (b.data() != &a[0]) err++;
         }
         {
-          Kokkos::View<double*, Kokkos::MemoryUnmanaged> b(a, 3);
+          Kokkos::View<double*, TEST_EXECSPACE, Kokkos::MemoryUnmanaged> b(a,
+                                                                           N);
           if (b.data() != &a[0]) err++;
         }
         {
-          Kokkos::View<const double*> b(a, 3);
+          Kokkos::View<const double*, TEST_EXECSPACE> b(a, N);
           if (b.data() != &a[0]) err++;
         }
         {
-          Kokkos::View<const double*, Kokkos::MemoryUnmanaged> b(a, 3);
+          Kokkos::View<const double*, TEST_EXECSPACE, Kokkos::MemoryUnmanaged>
+              b(a, N);
           if (b.data() != &a[0]) err++;
         }
       },
@@ -86,22 +90,24 @@ void from_custom_ptr() {
   Kokkos::parallel_reduce(
       Kokkos::RangePolicy(TEST_EXECSPACE(), 0, 1),
       KOKKOS_LAMBDA(int, int& err) {
-        double data[3];
+        double data[N];
         CustomPtr a{&data[0]};
         {
-          Kokkos::View<double*> b(a, 3);
+          Kokkos::View<double*, TEST_EXECSPACE> b(a, N);
           if (b.data() != &a[0]) err++;
         }
         {
-          Kokkos::View<double*, Kokkos::MemoryUnmanaged> b(a, 3);
+          Kokkos::View<double*, TEST_EXECSPACE, Kokkos::MemoryUnmanaged> b(a,
+                                                                           N);
           if (b.data() != &a[0]) err++;
         }
         {
-          Kokkos::View<const double*> b(a, 3);
+          Kokkos::View<const double*, TEST_EXECSPACE> b(a, N);
           if (b.data() != &a[0]) err++;
         }
         {
-          Kokkos::View<const double*, Kokkos::MemoryUnmanaged> b(a, 3);
+          Kokkos::View<const double*, TEST_EXECSPACE, Kokkos::MemoryUnmanaged>
+              b(a, N);
           if (b.data() != &a[0]) err++;
         }
       },
@@ -114,22 +120,76 @@ void from_ptr() {
   Kokkos::parallel_reduce(
       Kokkos::RangePolicy(TEST_EXECSPACE(), 0, 1),
       KOKKOS_LAMBDA(int, int& err) {
-        double a[3];
+        double a[N];
         {
-          Kokkos::View<double*> b(&a[0], 3);
+          Kokkos::View<double*, TEST_EXECSPACE> b(&a[0], N);
           if (b.data() != &a[0]) err++;
         }
         {
-          Kokkos::View<double*, Kokkos::MemoryUnmanaged> b(&a[0], 3);
+          Kokkos::View<double*, TEST_EXECSPACE, Kokkos::MemoryUnmanaged> b(
+              &a[0], N);
           if (b.data() != &a[0]) err++;
         }
         {
-          Kokkos::View<const double*> b(&a[0], 3);
+          Kokkos::View<const double*, TEST_EXECSPACE> b(&a[0], N);
           if (b.data() != &a[0]) err++;
         }
         {
-          Kokkos::View<const double*, Kokkos::MemoryUnmanaged> b(&a[0], 3);
+          Kokkos::View<const double*, TEST_EXECSPACE, Kokkos::MemoryUnmanaged>
+              b(&a[0], N);
           if (b.data() != &a[0]) err++;
+        }
+      },
+      errors);
+  ASSERT_EQ(errors, 0);
+}
+
+std::nullptr_t& nullptr_ref(std::nullptr_t& val) { return val; }
+
+void from_nullptr_and_0() {
+  int errors = 0;
+  Kokkos::parallel_reduce(
+      Kokkos::RangePolicy(TEST_EXECSPACE(), 0, 1),
+      KOKKOS_LAMBDA(int, int& err) {
+        {
+          Kokkos::View<double*, TEST_EXECSPACE> b(nullptr, 0);
+          if (b.data() != nullptr) err++;
+        }
+        {
+          Kokkos::View<double*, TEST_EXECSPACE, Kokkos::MemoryUnmanaged> b(
+              nullptr, 0);
+          if (b.data() != nullptr) err++;
+        }
+        // need a temporary so we can test reference to nullptr_t
+        // you can't get a non-const ref to nullptr (the variable)
+        std::nullptr_t nullptrtmp;
+        {
+          Kokkos::View<const double*, TEST_EXECSPACE> b(nullptr_ref(nullptrtmp),
+                                                        0);
+          if (b.data() != nullptr) err++;
+        }
+        {
+          Kokkos::View<const double*, TEST_EXECSPACE, Kokkos::MemoryUnmanaged>
+              b(nullptr_ref(nullptrtmp), 0);
+          if (b.data() != nullptr) err++;
+        }
+        {
+          Kokkos::View<const double*, TEST_EXECSPACE> b(0, 0);
+          if (b.data() != nullptr) err++;
+        }
+        {
+          Kokkos::View<const double*, TEST_EXECSPACE, Kokkos::MemoryUnmanaged>
+              b(0, 0);
+          if (b.data() != nullptr) err++;
+        }
+        {
+          Kokkos::View<const double*, TEST_EXECSPACE> b(NULL, 0);
+          if (b.data() != nullptr) err++;
+        }
+        {
+          Kokkos::View<const double*, TEST_EXECSPACE, Kokkos::MemoryUnmanaged>
+              b(NULL, 0);
+          if (b.data() != nullptr) err++;
         }
       },
       errors);
@@ -141,6 +201,7 @@ TEST(TEST_CATEGORY, view_ctor_ptr_convertible) {
   from_carray();
   from_custom_ptr();
   from_ptr();
+  from_nullptr_and_0();
 }
 
 }  // namespace
