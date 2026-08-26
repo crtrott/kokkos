7fc79e502fbabf2552378d9b5174783b4042f2e5
Carl William Pearson <cwpears@sandia.gov>
2026-08-26 13:42:01 -0600

    TestViewCtorDataHandle.hpp: fix mangled static_assert
    Signed-off-by: Carl William Pearson <cwpears@sandia.gov>

--- core/unit_test/view/TestViewCtorDataHandle.hpp
+++ core/unit_test/view/TestViewCtorDataHandle.hpp
@@ -40,13 +40,6 @@ struct TestViewCtorDataHandle {
 
     // This is not currently supported but may be added in the future
     //{
-    // Kokkos::View<int *> b(a.data_handle(), 5);
-    // ASSERT_EQ(a.use_count(), 2);
-    // ASSERT_EQ(b.use_count(), 2);
-    //}
-    static_assert(!std::constructible_from<Kokkos::View<int *>,
-    // This is not currently supported but may be added in the future
-    //{
     // Kokkos::View<int *, DeviceType> b(a.data_handle(), 5);
     // ASSERT_EQ(a.use_count(), 2);
     // ASSERT_EQ(b.use_count(), 2);
